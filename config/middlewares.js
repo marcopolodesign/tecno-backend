572096e758c918d86470f35d3e3a2f497670559b
Mat Aldao <m@marcopolo.agency>
2025-10-28 14:33:39 -0300

    Pushing

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,38 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+        "img-src": [
+          "'self'",
+          "data:",
+          "blob:",
+          "strapi.io",
+          "sparring.s3.nyc3.cdn.amazonaws.com",
+          "https://sparring.s3.nyc3.cdn.amazonaws.com",
+          "sparring.nyc3.digitaloceanspaces.com", 
+          "https://sparring.nyc3.digitaloceanspaces.com" 
+        ],
+        "media-src": [
+          "'self'",
+          "data:",
+          "blob:",
+          "strapi.io",
+          "sparring.s3.nyc3.cdn.amazonaws.com",
+          "https://sparring.s3.nyc3.cdn.amazonaws.com",
+          "sparring.nyc3.digitaloceanspaces.com", 
+          "https://sparring.nyc3.digitaloceanspaces.com"
+        ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- config/server.js
+++ config/server.js
@@ -1,16 +1,12 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('APP_URL', ''),
-  proxy: true,
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  admin: {
-    url: env('PUBLIC_ADMIN_URL', '/admin'),
-    auth: {
-      secret: env('ADMIN_JWT_SECRET'),
-    },
+  cron : {
+    enabled: true, 
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
